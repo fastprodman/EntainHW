bf83662be531007d9f9d1f9e13b7606de8efa9e9
fastprodman <stas.jurgensons@gmail.com>
2025-08-16 15:24:47 +0300

    fix: Reduce to passing dsn

--- cmd/api/config.go
+++ cmd/api/config.go
@@ -1,28 +1,15 @@
 package main
 
-import "fmt"
-
 type apiConfig struct {
 	Port     string
 	Postgres PostgresConf
 }
 
 type PostgresConf struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	DBName   string
+	DSN string
 }
 
 func readConfig() (*apiConfig, error) {
 	// TODO: add config reading logic
 	return &apiConfig{}, nil
-}
-
-func (pc PostgresConf) DSN() string {
-	return fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		pc.User, pc.Password, pc.Host, pc.Port, pc.DBName,
-	)
 }
